d9a27698427ae7d4df28b277c0208dc42f13e729
christian williams <christian.williamsj@gmail.com>
2019-12-03 04:49:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+
+  attr_accessor  :id, :name, :type, :db
+
+    def initialize(keywords)
+
+    end
+
+    def self.save(name, type, db)
+      db.execute("INSET INTO pokemon(name, type) VALUES (?, ?)",name, type)
+    end
 end
